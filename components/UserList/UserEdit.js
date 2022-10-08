355cd0105f94b27dc669444cb37bb3e586242c88
최지호 <gh9712@gmail.com>
2022-10-08 12:55:18 +0900

    회원수정화면 구현 (프로필이미지 수정)

--- components/UserList/UserEdit.js
+++ components/UserList/UserEdit.js
@@ -8,8 +8,28 @@ import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
 import TextField from '@mui/material/TextField';
 import OutlinedInput from '@mui/material/OutlinedInput';
-function UserEdit({inputs:{username,profileImageUrl}, onChange}){
+import { BASIC_CONSTANT } from "../../variables/basic.constants";
+import { useFetchWrapper } from '../../helpers';
 
+function UserEdit({inputs:{username,profileImageUrl}, onChange}){
+    const fetchWrapper = useFetchWrapper();
+    const [image, setImage] = React.useState(profileImageUrl);
+    const onLoadFile = (e) => {
+        const formData = new FormData();
+        formData.append('multipartFile',e.target.files[0])
+        fetchWrapper.post(`${BASIC_CONSTANT.BACKEND_URL}/image`,formData,'formData')
+        .then(response=>{
+            if(response[0]){
+                setImage(`https://api.sanpa.co.kr/image?name=${response[0]}`);
+                onChange({
+                    target:{
+                        name:'profileImageUrl',
+                        value:`https://api.sanpa.co.kr/image?name=${response[0]}`
+                    }
+                })
+            }
+        })
+    }
     return(
     <div className="card-body">
         <form>
@@ -18,8 +38,14 @@ function UserEdit({inputs:{username,profileImageUrl}, onChange}){
                 <input name="username" type="text" placeholder="이름을 입력해주세요." onChange={onChange} value={username} className={`form-control`} />
             </div>
             <div className="form-group">
-                <label>프로필 이미지 url</label>
-                <input name="profileImageUrl" type="text" placeholder="프로필 이미지 url을 입력해주세요." onChange={onChange} value={profileImageUrl} className={`form-control`} />
+                <label>프로필 이미지</label>
+                <input 
+                type="file" 
+                style={{display:"block"}} 
+                accept="img/*"
+                onChange={onLoadFile}
+                />
+                <img style={{marginTop:`${10}px`}} src={image}/>
             </div>
         </form>
     </div>
